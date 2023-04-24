d51e545eeb1e50708c38835a95ddccdad37dd4dc
Shilpa M L <shilpaml@ShilpaML-iMac.local>
2023-04-24 10:20:24 +0530

    CourseDetail

---
+++ src/components/myCourse/courseDetail/CourseDetail.jsx
@@ -0,0 +1,12 @@
+import './CourseDetail.css';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const CourseDetail = () => {
+  console.log('my course');
+  return (
+    <div >CourseDetail
+    </div>
+  );
+};
+
+export default CourseDetail;
---
+++ src/components/myCourse/courseDetail/rightCourseDetail/DummyFile.jsx
@@ -0,0 +1,12 @@
+// import './CourseDetail.css';
+import { NavLink, Outlet } from 'react-router-dom';
+
+const DummyFile = () => {
+  console.log('my course');
+  return (
+    <div >DummyFile
+    </div>
+  );
+};
+
+export default DummyFile;
